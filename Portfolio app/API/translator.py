64a1a435c9ce5a7487fa166b452924c17b5710de
Athira Lakshmi P <athiralakshmi.prakash@gmail.com>
2023-03-20 23:46:31 +0530

    created api , defined api function

--- Portfolio app/API/translator.py
+++ Portfolio app/API/translator.py
@@ -5,4 +5,12 @@ from flask import Flask , render_template
  @app.route('/')
  def home():
     return render_template("home.html")
+ 
+ 
+ @app.route('/api/v1/word')
+ def api(word):
+     uppercase=word.Title()
+     result_dictionary={"word":word,"Uppercase":uppercase}
+     return result_dictionary
+ 
 
